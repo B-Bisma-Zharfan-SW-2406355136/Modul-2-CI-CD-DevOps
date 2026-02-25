f2d854fa5c9fe718cefbf3af2ced501e80d0a5ec
satriobismaa <bismazharfan@gmail.com>
2026-02-25 23:25:21 +0700

    fix: add dependencies thymeleaf for open the page

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -55,7 +55,7 @@ class CarController extends ProductController{
     @PostMapping("/createCar")
     public String createCarPost(@ModelAttribute Car car, Model model){
         carService.create(car);
-        return "redirect:listCar";
+        return "redirect:/car/listCar";
     }
 
     @GetMapping("/listCar")
@@ -76,12 +76,12 @@ class CarController extends ProductController{
     public String editCarPost(@ModelAttribute Car car, Model model){
         System.out.println(car.getCarId());
         carService.update(car.getCarId(), car);
-        return "redirect:listCar";
+        return "redirect:/car/listCar";
     }
 
     @PostMapping("/deleteCar")
     public String deleteCar(@RequestParam("carId") String carId){
         carService.deleteCarById(carId);
-        return "redirect:listCar";
+        return "redirect:/car/listCar";
     }
 }
